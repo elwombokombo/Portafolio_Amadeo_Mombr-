3a456ed11d02e0ce2bb22787bcde9a98c64bab41
Amadeo Mombrú <santiagoamadeo123@gmail.com>
2025-06-11 17:19:47 -0300

    UT1 Parte 1 y Parte 2 hasta el ejercicio 3

---
+++ UT1/PD1/Parte2/ut1_parte2/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Multsuma.multsuma(1.0, 2.0, 3.0);
+    }
+}
---
+++ UT1/PD1/Parte2/ut1_parte2/src/main/java/com/example/Multsuma.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Multsuma {
+    public static void multsuma(double a, double b, double c){
+        double resultado=a*b+c;
+        System.out.println(resultado);
+    }
+    
+}
